50158faad0208e9fad55c361911511f92dcecf97
Agenaeu <sdrice18@gmail.com>
2018-08-23 20:10:07 -0700

    js working, working on css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,17 @@
 //on load function
 $(document).ready(function() {
     
-        
+    // global variables
     var score = 0;
     var wins = 0;
-    var losses =0;
+    var losses = 0;
+    // sets random numbers for crystal
     var amethystNum = Math.floor((Math.random()*12)+1);
     var sapphireNum = Math.floor((Math.random()*12)+1);
     var emeraldNum = Math.floor((Math.random()*12)+1);
+    // sets random number to try and reach
     var randomNum = Math.floor((Math.random()*102)+19);
-    console.log(randomNum);
-    console.log(score);
+    // makes the crystals unable to = the same number as each other
     if (amethystNum === sapphireNum || amethystNum === emeraldNum) {
         sapphireNum = Math.floor((Math.random()*12)+1);
         emeraldNum = Math.floor((Math.random()*12)+1);  
@@ -18,47 +19,76 @@ $(document).ready(function() {
     if (sapphireNum === emeraldNum) {
         emeraldNum = Math.floor((Math.random()*12)+1);  
     }
+    // resets the score and crystal hidden values
+    var resetGame = function() {
+         score = 0;
+         amethystNum = Math.floor((Math.random()*12)+1);
+         
+         sapphireNum = Math.floor((Math.random()*12)+1);
+        
+         emeraldNum = Math.floor((Math.random()*12)+1);
+         
+         randomNum = Math.floor((Math.random()*102)+19);
+         $("#randomNum").html("<h3>Random Number: " + randomNum + "</h3>");
+         $("#score").html("<h4>Your total score: " + score + "</h4>");
+    };
     
-    
+    // writes the score you are trying to reach to the HTML
     $("#randomNum").html("<h3>Random Number: " + randomNum + "</h3>");
-    
+    // click function for the amethyst
     $("#amethyst").click(function(){
         /* alert("you clicked amethyst"); */
         score += amethystNum;
-        /* console.log(score); */
-        $("#score").html("<h4>Your total score: " + score + "</h4>");
-        /* if (score === randomNum) {
-            wins++;
-            alert("You win!");
-        } */
-
         
-    });
-    $("#sapphire").click(function(){
-        /* alert("you clicked sapphire"); */
-      newScore = score += sapphireNum;
-        /* console.log(score); */
         $("#score").html("<h4>Your total score: " + score + "</h4>");
+        if (score === randomNum) {
+            wins++;
+            $("#wins").html("<h6>Wins: " + wins + "</h6>");
+            resetGame();
+            
+        }
+        if (score > randomNum) {
+            losses++;
+            $("#losses").html("<h6>Losses: " + losses + "</h6>");
+            resetGame();
+            
+        }
     });
+    // click function for the sapphire
+    $("#sapphire").click(function(){
+        
+      newScore = score += sapphireNum;
+      
+        $("#score").html("<h4>Your total score: " + score + "</h4>");
+        if (score === randomNum) {
+            wins++;
+            $("#wins").html("<h6>Wins: " + wins + "</h6>");
+            resetGame();
+        }
+        if (score > randomNum) {
+            losses++;
+            $("#losses").html("<h6>Losses: " + losses + "</h6>");
+            resetGame();
+        }
+    });
+    // click function for emerald
     $("#emerald").click(function(){
-        /* alert("you clicked emerald!"); */
+       
         newScore = score += emeraldNum;
-        /* console.log(score); */
-        $("#score").html("<h4>Your total score: "  + score + "</h4>");   
+   
+        $("#score").html("<h4>Your total score: "  + score + "</h4>"); 
+        if (score === randomNum) {
+            wins++;
+            $("#wins").html("<h6>Wins: " + wins + "</h6>");
+            resetGame();
+        }
+        if (score > randomNum) {
+            losses++;
+            $("#losses").html("<h6>Losses: " + losses + "</h6>");
+            resetGame();
+        }  
     });
 
-    /* console.log(newScore);
-    if (newScore === randomNum) {
-    alert("You win!");
-    wins++;   
-    $("#wins").html("<h6>Wins: " + wins + "</h6>");
-    randomNum();
-    }
-    else if (newScore > randomNum){
-    alert("You lose!");
-    losses++;
-    $("#losses").html("<h6>Losses: " + losses + "</h6>");
-    } */
 
 
     
